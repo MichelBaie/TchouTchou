8ac81beba4ae725104f70cf686c50713c942a8cd
Tryon <68423470+tryon-dev@users.noreply.github.com>
2023-02-07 21:46:43 +0100

    Update tchoutchou.sh

--- tchoutchou.sh
+++ tchoutchou.sh
@@ -21,7 +21,7 @@ echo "! une fois le train lancé, enfoncez CTRL+C pendant quelque seconde pour s
 read -p "appuyer sur ENTRER pour lancer le train"
 
 clear
-echo "Lancement de TchouTchou.sh (édition summer 2022)"
+echo "Lancement de TchouTchou.sh (édition hiver 2023)"
 sleep 1
 
 for (( ; ; ))
